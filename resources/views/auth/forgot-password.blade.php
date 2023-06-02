c16f1c38d997784c7364a029e3f7bec3a0c1f3e5
Arman Ali <mohammadarmanali.din@gmail.com>
2023-06-02 12:03:56 +0600

    fix: password reset fixed. now it is working good

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,8 +1,9 @@
+@section('title', 'Reset Password')
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
             </a>
         </x-slot>
 
